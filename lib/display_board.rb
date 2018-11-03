096e5f0817b6cc9796200b449124c756998113dc
STEPHANIE MESSAD kOUCHICH <sabud1208@gmail.com>
2018-11-03 18:15:23 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,11 +1,11 @@
 board = [" "," "," "," "," "," "," "," "," "]
 
 def display_board(board)
-  puts board[0]= ["   |   |   "]
-  puts board[1]= ["-----------"]
-  puts board[2]= ["   |   |   "]
-  puts board[3]= ["-----------"]
-  puts board[4]= ["   |   |   "]
+  puts board[0]= "   |   |   "
+  puts board[1]= "-----------"
+  puts board[2]= "   |   |   "
+  puts board[3]= "-----------"
+  puts board[4]= "   |   |   "
 end
 
-print display_board(board[2] << "x")
+display_board(board[2] << "x")
