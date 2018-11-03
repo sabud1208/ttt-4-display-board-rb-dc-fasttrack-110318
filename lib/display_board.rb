3f3da1e261f2824e7a666536cc07e8bb194ddc36
STEPHANIE MESSAD kOUCHICH <sabud1208@gmail.com>
2018-11-03 18:03:48 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,11 +1,11 @@
 board = [" "," "," "," "," "," "," "," "," "]
 
 def display_board(board)
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
+  puts row1= "   |   |   "
+  puts rowl= "-----------"
+  puts row2= "   |   |   "
+  puts rowi= "-----------"
+  puts row3= "   |   |   "
 end
 
-display_board(board[5]= "X")
+display_board(row2[5]= "X")
